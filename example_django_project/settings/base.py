774a237a3ca2239191498429879cf2da0f69840f
willover <z_git@systems.co.uk>
2015-10-26 17:29:17 -0700

    STATIC configuration iteration

--- example_django_project/settings/base.py
+++ example_django_project/settings/base.py
@@ -113,13 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static1/'),
 )
 
 
