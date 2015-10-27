a66d4159648831e0fa79b3aa96263d39e7303542
willover <z_github.com@systems.co.uk>
2015-10-27 01:45:17 +0000

    Update urls.py

--- example_django_project/urls.py
+++ example_django_project/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^passreset/$',auth_views.password_reset,name='forgot_password1'),
+    url(r'^passresetdone/$',auth_views.password_reset_done,name='forgot_password2'),
+    url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,name='forgot_password3'),
+    url(r'^passresetcomplete/$',auth_views.password_reset_complete,name='forgot_password4'),
+    
+    url(r'^password/change/$',auth_views.password_change,name='auth_password_change'),
+    url(r'^password/change/done/$',auth_views.password_change_done,name='auth_password_change_done'),
 ]
