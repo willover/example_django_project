35a413fb2261d61e15836bb9a7413c69a0dd8426
willover <z_github.com@systems.co.uk>
2015-10-27 01:56:47 +0000

    Update urls.py

--- example_django_project/urls.py
+++ example_django_project/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'example_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
     url(r'^admin/', include(admin.site.urls)),
-    
-    url(r'^passreset/$',auth_views.password_reset,name='forgot_password1'),
-    url(r'^passresetdone/$',auth_views.password_reset_done,name='forgot_password2'),
-    url(r'^passresetconfirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',auth_views.password_reset_confirm,name='forgot_password3'),
-    url(r'^passresetcomplete/$',auth_views.password_reset_complete,name='forgot_password4'),
-    
-    url(r'^password/change/$',auth_views.password_change,name='auth_password_change'),
-    url(r'^password/change/done/$',auth_views.password_change_done,name='auth_password_change_done'),
 ]
