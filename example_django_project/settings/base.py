51fa1d502bf294229bafb9daed583b2c045e842c
willover <z_git@systems.co.uk>
2015-10-26 17:08:02 -0700

    deal with STATIC_ROOT absent error

--- example_django_project/settings/base.py
+++ example_django_project/settings/base.py
@@ -113,6 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Additional locations of static files
